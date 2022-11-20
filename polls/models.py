cc9e472a48bf8935ca218a804086aa39e1bce3c8
Oscar Parent <oscar.parent@student.unamur.be>
2022-11-20 17:37:47 +0100

    changement models.py

--- polls/models.py
+++ polls/models.py
@@ -3,12 +3,30 @@ import datetime
 from django.utils import timezone
 
 class Product(models.Model):
+
+    class EcoScore(models.TextChoices):
+        A = 'A'
+        B = 'B'
+        C = 'C'
+        D = 'D'
+        E = 'E'
+
+    class Category(models.TextChoices):
+        Fruits = 'Fruits'
+        Vegetables = 'Légumes'
+        Starch = 'Féculents'
+        Meat = 'Viande'
+        Drinks = 'Boissons'
+        ElseFood = 'Autres aliments'
+        Others = 'Autres'
+
+
     reference = models.CharField(max_length=100, null=False)
-    price = models.IntegerField(default=0, null=False)
+    price = models.FloatField(default=0, null=False)
     productor_name = models.CharField(max_length=200, default='No data for the productor name')
     entitled = models.CharField(max_length=200, null=False)
-    category = models.CharField(max_length=100, default="No category")
-    eco_score = models.CharField(max_length=1)
+    category = models.CharField(max_length=100, choices=Category.choices)
+    eco_score = models.CharField(max_length=1, choices=EcoScore.choices)
 
     def __str__(self):
         return self.reference
@@ -38,4 +56,3 @@ class Shop(models.Model):
 
     def __str__(self):
         return self.shop_name
-
