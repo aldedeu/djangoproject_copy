251882907d92ae153aa07469638eeea2781efe78
bitazur <118678132+bitazur@users.noreply.github.com>
2022-11-20 17:31:22 +0100

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 import datetime
 from django.utils import timezone
-
+#coucou
 class Product(models.Model):
     reference = models.CharField(max_length=100, null=False)
     price = models.IntegerField(default=0, null=False)
