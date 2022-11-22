f1d903d798a49acd102fd057a492e0cbbf6fbc23
aldedeu <116264613+aldedeu@users.noreply.github.com>
2022-11-22 16:50:22 +0100

    ajout vue

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Product, User, Shop
 
 def index(request):
-    return HttpResponse("Hello World ! You're at the polls index :)")
-
-
+    display_products = Product.objects.all()
+    template = loader.get_template('Display_products.html')
+    context = {'display_products': display_products}
+    return HttpResponse(template.render(context, request))
