65b45330a3c9d74291869bbc0148bcfcc3f24d62
aldedeu <116264613+aldedeu@users.noreply.github.com>
2022-11-26 21:24:19 +0100

    ajout de la vue detailShop
    Le if et else sont surtout là pour essayer de comprendre ce qui n'allait pas dans le code. En fait ça affiche bien les caractéristiques des magasins donc trop cool, mais ça fonctionne juste pas pour les produits... Essaie comme ça tu verras

--- polls/views.py
+++ polls/views.py
@@ -27,6 +27,18 @@ def Shops(request):
     print(context)
     return HttpResponse(template.render(context, request))
 
+def detailShop(request, shop_reference):
+    details = Shop.objects.get(reference_shop=shop_reference)
+
+    if details.reference_product == "GreenCompare.Product.None":
+        products_in_shop = None
+    else:
+        products_in_shop = 'f'
+
+    template = loader.get_template("Detail_shop.html")
+    context = {'details': details, 'products_in_shop': products_in_shop}
+    return HttpResponse(template.render(context, request))
+
 def LogIn(request):
     username = request.POST['username']
     password = request.POST['password']
