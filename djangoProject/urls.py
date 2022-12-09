6d05558d3d260e715bf74461d3d075ecd4fc34e1
aldedeu <116264613+aldedeu@users.noreply.github.com>
2022-12-09 14:50:43 +0100

    Update urls.py

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', GreenCompare.views.registerPage, name='register')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
